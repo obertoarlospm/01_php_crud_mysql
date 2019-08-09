a21a158936ea1bc4f25e8f0f982b0b3ca5031b61
obertoarlospm <53628288+obertoarlospm@users.noreply.github.com>
2019-08-08 21:02:56 -0500

    Add files via upload

---
+++ includes/header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>PHP MYSQL CRUD</title>
+
+    <!--BOOTSTRAP 4 - CSS-->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <!-- FONT AWESOME 5 - para iconos, ... -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.10.1/css/all.css" integrity="sha384-wxqG4glGB3nlqX0bi23nmgwCSjWIW13BdLUEYC4VIMehfbcro/ATkyDsF/AbIOVe" crossorigin="anonymous">
+</head>
+<body>
+
+<nav class="navbar navbar-dark bg-dark">
+    <div class="container">
+        <!-- <a href="index.php" class="navbar-brand">PHP MYSQL CRUD</a> -->
+        <a href="/01_php_crud_mysql" class="navbar-brand">PHP MYSQL CRUD</a>
+    </div>
+</nav>
